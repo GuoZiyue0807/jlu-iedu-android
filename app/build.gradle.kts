3916ece189c8da94163503d64938f6ece0a80c28
FlySky <1978756594@qq.com>
2026-06-12 14:07:57 +0800

    Add Android app Gradle config

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "cn.edu.jlu.iedu"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "cn.edu.jlu.iedu"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+}
